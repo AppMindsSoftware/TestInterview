bac32b23cc4f401aa8fe031f021cba9491813df0
rizesony <rizesony@gmail.com>
2020-02-06 20:04:33 +0530

    Commit

--- app/src/main/java/com/example/bhavanitest/SplashActivity.java
+++ app/src/main/java/com/example/bhavanitest/SplashActivity.java
@@ -128,7 +128,6 @@ public class SplashActivity extends Activity
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
             Log.v("TAG","Permission: "+permissions[0]+ "was "+grantResults[0]);
-            Log.v("TAG","Permission: "+permissions[0]+ "was "+grantResults[0]);
             //resume tasks needing this permission
         }
     }
